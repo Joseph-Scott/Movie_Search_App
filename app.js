046376442d939b3bfe0dce4a5c7856a8ccccfb23
Joe Scott <jbs198585@gmail.com>
2019-08-24 11:24:04 -0400

    get request for results

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-
+app.get("/results", function(req, res){
+  request("http://omdbapi.com/?s=georgia&apikey=thewdb", function(error, response, body){
+    if(!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("Movie App has started!!");
